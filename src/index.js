c68ec8659722fb92efe1887d7ec008538c3d40f3
Val Do <valeri.kharitonashvili1@gmail.com>
2022-11-25 19:30:20 +0400

    fix

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const element = (
-  <div>
-    <h1>Hello!</h1>
-    <h2>Good to see you here.</h2>
-  </div>
-);
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  root.render(element);
+}
 
-root.render(element);
+setInterval(tick, 1000);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
