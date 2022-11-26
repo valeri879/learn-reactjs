2f2480a958e044296d0d929c6364fba8def2b075
Val Do <valeri.kharitonashvili1@gmail.com>
2022-11-26 20:41:33 +0400

    components and props

--- src/index.js
+++ src/index.js
@@ -4,18 +4,14 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  root.render(element);
+function Welcome(props) {
+	console.log(props);
+	return <h1>Hello, {props.name} {props.lastName}</h1>;
 }
 
-setInterval(tick, 1000);
+const element = <Welcome lastName="Nickolson" name="Sara" />;
 
+root.render(element);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
